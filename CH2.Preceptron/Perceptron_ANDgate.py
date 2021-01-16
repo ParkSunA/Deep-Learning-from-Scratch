2ebea6f16e850a6875ade9eda32a8b14f4594e31
SunA Park <53106696+ParkSunA@users.noreply.github.com>
2021-01-16 22:32:11 +0900

    update

--- CH2.Preceptron/Perceptron_ANDgate.py
+++ CH2.Preceptron/Perceptron_ANDgate.py
@@ -13,3 +13,22 @@ print(AND(0,0))
 print(AND(1,0))
 print(AND(0,1))
 print(AND(1,1))
+
+# 가중치와 편향 도입
+# theta = -b 로 치환
+# AND gate의 perceptron 코드는 다음과 같이 변한다
+import numpy as np
+
+def AND(x1,x2):
+    x = np.array([x1,x2])
+    w = np.array([0.5,0.5])
+    b = -0.7
+    tmp = np.sum(w*x)+b
+    if tmp <=0:
+        return 0
+    else:
+        return 1
+print(AND(0,0))
+print(AND(1,0))
+print(AND(0,1))
+print(AND(1,1))
