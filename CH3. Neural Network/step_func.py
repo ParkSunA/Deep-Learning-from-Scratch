9fc0835cc1b69033a21a5a13279409ad3d3ab3c3
SunA Park <53106696+ParkSunA@users.noreply.github.com>
2021-01-26 08:49:07 +0900

    Add files via upload

---
+++ CH3. Neural Network/step_func.py
@@ -0,0 +1,22 @@
+def step_function(x):
+    if x > 0:
+        return 1
+    else:
+        return 0
+# step_function(x)은 실수만 입력 가능하다. 즉 배열을 인수 x로 넣을 수 없다.
+# step_function(3.0) OK, step_function(np.array([1,2])) Not OK
+# 넘파이 배열도 지원 가능하게 수정하면 다음과 같다.
+
+import numpy as np
+
+def step_function(x):
+    y = x > 0
+    return y.astype(np.int)
+
+# 이렇게 간단하게 구현할 수 있는 이유는 넘파이 배열을 인풋으로 받기 때문이다.
+x = np.array([-1,1.0,2.0])
+print(x)
+y = x > 0
+print(y)
+print(y.astype(np.int))
+print(step_function(x))
